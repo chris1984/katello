1f59960ec357c64c07a61a7aa5e892e540ea361e
Chris Roberts <chrobert@redhat.com>
2024-09-26 15:23:41 -0400

    Refs #37865 - Turn off required for name in AK update and add rabl for multi env

--- app/controllers/katello/api/v2/activation_keys_controller.rb
+++ app/controllers/katello/api/v2/activation_keys_controller.rb
@@ -16,7 +16,6 @@ module Katello
 
     def_param_group :activation_key do
       param :organization_id, :number, :desc => N_("organization identifier"), :required => true
-      param :name, String, :desc => N_("name"), :required => true
       param :description, String, :desc => N_("description")
       param :max_hosts, :number, :desc => N_("maximum number of registered content hosts")
       param :unlimited_hosts, :bool, :desc => N_("can the activation key have unlimited hosts")
@@ -62,6 +61,7 @@ module Katello
     end
 
     api :POST, "/activation_keys", N_("Create an activation key")
+    param :name, String, :desc => N_("name"), :required => true
     param_group :activation_key
     def create
       @activation_key = ActivationKey.new(activation_key_params) do |activation_key|
@@ -78,6 +78,7 @@ module Katello
     api :PUT, "/activation_keys/:id", N_("Update an activation key")
     param_group :activation_key
     param :id, :number, :desc => N_("ID of the activation key"), :required => true
+    param :name, String, :desc => N_("name"), :required => false
     def update
       @activation_key.update!(content_view_environments: @content_view_environments) if @content_view_environments.present?
       sync_task(::Actions::Katello::ActivationKey::Update, @activation_key, activation_key_params)
